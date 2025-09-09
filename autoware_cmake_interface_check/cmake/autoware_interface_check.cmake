8d6d6217983145d3fee69cbcb112859e614763cf
Takagi, Isamu <isamu.takagi@tier4.jp>
2025-09-09 19:46:24 +0900

    add cspell:ignore
    Signed-off-by: Takagi, Isamu <isamu.takagi@tier4.jp>

--- autoware_cmake_interface_check/cmake/autoware_interface_check.cmake
+++ autoware_cmake_interface_check/cmake/autoware_interface_check.cmake
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# cspell:ignore ARGN, TESTNAME
+
 function(autoware_interface_check)
 
   # Parse arguments and set default value.
--- autoware_interface_check/autoware_interface_check/entrypoint.py
+++ autoware_interface_check/autoware_interface_check/entrypoint.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# cspell:ignore addindent, newl
+
 import argparse
 import textwrap
 import time
